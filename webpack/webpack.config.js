39a1112e1b2f5e71da81229eae14726719d9edf0
Kim Jae Hoon <hoon.development@gmail.com>
2022-08-20 18:49:00 +0900

    feat: 웹팩 기본 설정 (입출력 및 로더)

---
+++ webpack/webpack.config.js
@@ -0,0 +1,35 @@
+const Path = require("path");
+
+const version = process.env.npm_package_version;
+
+module.exports = {
+  entry: {
+    eme: Path.resolve(__dirname, "../src/index.ts")
+  },
+  output: {
+    filename: `[name].${version}.js`,
+    path: Path.resolve(__dirname, "../dist"),
+    environment: {
+      arrowFunction: false,
+      bigIntLiteral: false,
+      const: false,
+      destructuring: false,
+      dynamicImport: false,
+      forOf: false,
+      module: false,
+      optionalChaining: false,
+      templateLiteral: false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+}
