5d587af9c94b10f9052fd962e071dcb96781d429
Kim Jae Hoon <hoon.development@gmail.com>
2022-08-20 18:50:15 +0900

    feat: 웹팩 개발용 설정, 데브 서버 추가

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,25 @@
+const Path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  ...require("./webpack.config"),
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: Path.resolve(__dirname, "../src/index.html")
+    })
+  ],
+  devServer: {
+    port: 8080,
+    liveReload: true,
+    client: {
+      overlay: true
+    },
+    static: [
+      {
+        directory: Path.join(__dirname, "../dist")
+      }
+    ]
+  }
+}
